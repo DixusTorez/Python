3b1e09ffe5687b8c754ca0e62bf1a05b1023eec2
DixusTorez <jpuen305@evit.edu>
2025-08-07 10:22:21 -0700

    Update unamed.py

--- unamed.py
+++ unamed.py
@@ -1,12 +1,19 @@
-print("""Who am I you may be asking? You are the legendary Gastillo of Kaizoku.
+print("Gastillo opens his eyes — all 16 of them.")
+print("He hears the fans in his head whirring as the implanted Spyder_Eys calibrate.")
+print("The motion is nearly sickening, but he gets used to it.")
 
-Description: Gastillo is of Mexican descent. He stands at a whopping six foot two and two hundred pounds. He has brown hair which he styles in a buzz cut. He has a multitude of augments, some of the more noticeable augments are Spyder_Eys that add black dots to the front and back of his head. They give him the ability to see in infrared, night vision and thermal as well as the ability to zoom in on targets. Another noticeable augment is a Vocal Linguistic Box. This gives him the ability to speak in any dialect but with the drawback of voice glitches and scratches.
+print("\nLooking around, he realizes he's been in cryostasis for about ten years.")
+print("On the left side of the chamber, he sees a sticky note that reads: 'Open your codex.'")
 
-Gastillo opens his eyes all 16 of them. He hears the fans in his head whir while the implanted Spyder_Eys all around him calibrate. It's near sickening levels of motion sickness but soon he gets used to it. He looks around at where he was sleeping, he sees he's been in cryostasis for a while, around ten years. On the left in the plush of the chamber he sees a sticky note telling him to "open his codex." First he checks his shirt pockets and then his pants. Eventually he finds it in his boots. On the codex a bright message pops up from who, Gastillo assumes, one of his friends.
+print("\nHe checks his shirt pockets, then pants, and finally finds the codex in his boots.")
+print("A message appears on the screen:")
 
-"Hey man it's been a long slumber, you'll warm up eventually. You were sent here by the big man up top to take over this Vastella ship. You might've sustained some memory loss but that's okay. This codex should catch you up to speed. Or don't it doesn't really matter."
+print('''\n"Hey man, it's been a long slumber. You'll warm up eventually.
+You were sent here by the big man up top to take over this Vastella ship.
+You might’ve sustained some memory loss, but that’s okay.
+This codex should catch you up to speed. Or don’t — it doesn’t really matter."''')
 
-"CODEX:"
+print('''\n "CODEX:"
 The Kaizoku:
 
 Ships: The ships that you will be using are the Blugills and Mantisellas. Better learn how to fly fast
@@ -21,24 +28,57 @@ Buq'ah:
 Huge, fleeting ships are why they got their name. Primarily an offworld smuggling operation, they use Vastellas for everything. Our main and primary threat. It's kill on sight for these guys. They'll rep it in your face like nothing.
 
 Lasiks: Get your hands on one of these puppies. They're valuable, trustworthy and very beautiful. Just be careful with your hands they'll burn.
-
-""")
-
-print('''"Shit…” he says with an extremely crackly voice. Confused at the sound of his voice, he reaches up to touch his throat and face. He feels hard metal and wires slightly panicking; he takes a deep breath and thinks. It takes a few minutes but now he is starting to remember his implants”
-
 ''')
 
-print('''A lady comes by still seeing him in the cryo pod peeks in “Hey dude you can come out now, Do you need help?”''') 
+print('\n“Shit…” Gastillo mutters, his voice crackling with static.')
+print("Confused, he touches his throat and feels wires and metal. Panicked, he breathes deeply, regaining some memory of his implants.")
 
-answer = input("Yes or No? ")
+print('\nA woman appears, peeking into the cryopod.')
+print('"Hey dude, you can come out now. Do you need help?"')
 
-if answer.lower() == "yes":
-    print(''' "You step out with her, your hand around her shoulder steadying yourself."''' 
-    )
-else:
-    print('''"She pulls you out anyway, seeing your disheveled state. You step out with her reluctantly, your hand around her shoulder steadying yourself. “Yeah man, I'm helping whether or not you want it. My name is Joseph. Odd name for a woman so people just call me Jo."'''
-    )
-    
-print('''
+# First choice
+while True:
+    answer = input("\nYes or No? ").lower()
+    if answer == "yes":
+        print("You step out with her, your hand around her shoulder, steadying yourself.")
+        break
+    elif answer == "no":
+        print('''She pulls you out anyway, seeing your disheveled state.
+You step out with her reluctantly, your hand around her shoulder, steadying yourself.
+"Yeah man, I'm helping whether or not you want it. My name is Joseph. Odd name for a woman, so people just call me Jo."''')
+        break
+    else:
+        print("Invalid answer. Try again.")
 
-Jo looks at you semi intrested. "So whats your name?" she asks "my name? its Gastillo" you respond. she looks at you in awe "wow... i had no idea you were in to cybernetics. you have spider eyes and a vtb! so cool..." gastillo chuckles " yeah its... useful put a pain in the ass." gastillo looks around the ship "where are we going" Jo says "To the mess hall. you can eat with me" gastillo says " ill take you up on that"''')
+# Conversation continues
+print('\nJo looks at you, semi-interested.')
+print('"So, what’s your name?" she asks.')
+
+print('"My name? It’s Gastillo," you respond.')
+
+print('''\nShe looks at you in awe.
+"Wow… I had no idea you were into cybernetics. You have Spyder_Eys and a VTB! So cool..."''')
+
+print('\nGastillo chuckles. "Yeah, it’s... useful. But a pain in the ass."')
+
+print('''\nYou both begin walking down a flight of stairs. 
+The ship is massive — cryopods stacked to the ceiling, wide halls like a hotel.
+You zoom in with your Spyder_Eys, estimating the ship holds hundreds of thousands, maybe millions.''')
+
+print('\nJo notices you spacing out. "Hey dude, are you okay?"')
+
+print('"Oh yeah..." you say.')
+
+# Second choice
+while True:
+    answer = input("\nTell the truth? Yes or No? ").lower()
+    if answer == "yes":
+        print('''"I'm just looking for friends," you admit.''')
+        break
+    elif answer == "no":
+        print('"Oh, just looking around," you say, deflecting.')
+        break
+    else:
+        print('''She looks at you, confused.
+"Jeez man, are you sure your brain didn't freeze?"
+Invalid answer. Please try again.''')
