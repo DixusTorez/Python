4278276a3b50f5113020c6cdc397b0e14ddbd5f7
DixusTorez <jpuen305@evit.edu>
2025-08-07 09:17:23 -0700

    Update unamed.py

--- unamed.py
+++ unamed.py
@@ -1,38 +1,25 @@
-print('''"Who am i?" you may be asking? You are the legendary Gastillo of Kaizoku.
+print("""Who am I you may be asking? You are the legendary Gastillo of Kaizoku.
 
 Description: Gastillo is of Mexican descent. He stands at a whopping six foot two and two hundred pounds. He has brown hair which he styles in a buzz cut. He has a multitude of augments, some of the more noticeable augments are Spyder_Eys that add black dots to the front and back of his head. They give him the ability to see in infrared, night vision and thermal as well as the ability to zoom in on targets. Another noticeable augment is a Vocal Linguistic Box. This gives him the ability to speak in any dialect but with the drawback of voice glitches and scratches.
 
-Gastillo opens his eyes all 16 of them. He hears the fans in his head whir while the implanted spider eys all around him callibrate. Its near sickening levels of motion sickness but soon he gets used to it. he looks around at where he was sleeping, he sees he's been in cryostasis for a while around ten years. on the left in the plush of the chamber he sees a sticky note telling him to "open his codex". first he checks his shirt pockets and then his pants. eventually he finds it in his boots. on the codex a bright message pops up from who, gastillo assumes, one of his friends.
+Gastillo opens his eyes all 16 of them. He hears the fans in his head whir while the implanted spider eys all around him calibrate. It's near sickening levels of motion sickness but soon he gets used to it. He looks around at where he was sleeping, he sees he's been in cryostasis for a while, around ten years. On the left in the plush of the chamber he sees a sticky note telling him to "open his codex." First he checks his shirt pockets and then his pants. Eventually he finds it in his boots. On the codex a bright message pops up from who, Gastillo assumes, one of his friends.
 
-"hey man its been a Long slumber, youll warm up eventually. you were sent here by the big man up top to take over this vastella ship. you mightve substained some memory loss but thats okay. this codex should catch you up to speed. or dont it doesnt really matter."
+"Hey man it's been a long slumber, you'll warm up eventually. You were sent here by the big man up top to take over this Vastella ship. You might've sustained some memory loss but that's okay. This codex should catch you up to speed. Or don't it doesn't really matter."
 
 "CODEX:"
-The Kaizoku: They are the first people to have gone interplanetary (since the Drive Age)  in regards to smuggling operations. Legend surrounds them due to the fact that they are so prolific and extravagant in their operations, some of these legends include killing dozens of Sabrinen soldiers with every operation they undertake, Smuggling millions of people, being the sole creators of certain hyper drugs and being the only people to have the ability to fight the Sabrinen Empire. In reality they are usually quiet and in the background. Most recently they have almost been hunted out to extinction this is due to their perceived legendary nature. They still hold some claim to fame due to the simple fact that they are able to hold and maintain outposts on different planets.
-Leadership of the Kaizoku consists of a council of primarily men.
+The Kaizoku:
 
-Ships: The ships that you will be using are the ships a smuggler typically uses are Blugills and Mantisellas. 
+Ships: The ships that you will be using are the Blugills and Mantisellas.
 
-Mantisellas: these ships are fast, precise, and deadly. They look like flattened out hummingbirds. Primarily used by Sabrinen as suicide bombers or defenders. Also used by the smugglers for going fast across planets or to different planets. They are very customizable. For example, how the Sabrinen transforms it into a suicide ship is by adding explosives, a long needle and a welded shut cockpit. Smugglers change it to be used for speed only by taking out everything but the seat. Variants: Smuglsellas, Suicide Mantisellas and Sports mantisellas
+Mantisellas: They look like flattened out hummingbirds. Primarily used by Sabrinen as suicide bombers or defenders. Also used going fast across planets or to different planets. They are very customizable. But you aren't no mechanic.
 
-Blugills: aptly named for their cheapness, reliability, and adaptability,these are blugills. Boxy and decently fast, almost everyone from smugglers, civilians, and even officers use this. Usually used for planet travel, they can get anyone anywhere at a decent speed. It can hold a decent amount of cargo and is loved by many for customization. Variants: cargo blugills, suicide blugills, Retrival blugills and smuggler blugills You will encounter many more though. 
+Blugills: The best ship, fast, reliable and customizable. Always take one of these.
 
- Illicit Goods: The production and sale of illicit goods is a problem for all empires. Each empire has a different definition of illicit goods and therefore provides lists, checkpoints and some form of leeway. Some primary examples of illicit goods that are profoundly hated by all empires are: Lasik smuggling, any military hardware, people, and “Hyper drugs”. Obviously,you can get around it with the right bribes to the right people, driving around checkpoints, or just merking everyone at a checkpoint and going on through.
-
-“Gone Interplanetary”: This specific term refers to smugglers and other empires being able to get off their homeworld and move operations to other planets. With smugglers, this is typically a bad term because with the fame it gives it also gives you a target on your back. For empires, this gives access to opportunities that would never have been available otherwise,politically, culturally, money-wise wise and resource-wise.
-
-“Legend”:
- This term goes for anyone and anything. From ships, battles, wars, singular people, factions, and more, this is just a signifier that something has peaked and ingrained itself in a people's or empire's memory. Sometimes the ties go so deep it can make its way into history or even documents. 
-
-
-Smugglers:
-Factions within factions describe this faction. They mainly consist of AWOL conscripts and the occasional civilian. Many smugglers are there for riches and some but a rare few are there to, as genuinely as possible for a smuggler, help. Smugglers dip their toes into a multitude of "businesses" from trafficking to selling illicit goods as long as it pays there's an incentive. Typically smuggling is a manageable problem to deal with for a lot of political powers. Usually smuggling takes place on singular planets with weaker factions. The Kaizoku on the other hand have been dominating space. 
-
-Smugglers:
-Mente:
-Primarily fighters, they barely go off-world. They fight with anyone they come across, and they are general isolationists. They are primarily composed of platers. 
+Smugglers you'll likely come across:
 
 Buq'ah:
-Huge, fleeting ships are why they got their name. Primarily an offworld smuggling operation, they use Vastellas for everything. They utterly hate the Kaizuko and are constantly fighting with them. They seemingly appeared out of nowhere,making people wonder if the kaizuko were larger and had a split or if they were planted there by the Sabrinen Empire. Consists of the lando empires, people, and the noches.
+Huge, fleeting ships are why they got their name. Primarily an offworld smuggling operation, they use Vastellas for everything. Our main and primary threat. It's kill on sight for these guys. They'll rep it in your face like nothing.
 
-Lasiks:
-Considered the pinnacle of human technology, only overshadowed by the hyper drive, these are the most wanted, expensive and highly in demand weapons in the galaxy. They are a laser weapon which concentrates heat into a single focal point, and beams blast it out with mirrors at the desired target. These can take down ships, people and are used to cut holes, engineer, terraform and in some cases farm. The downsides mainly consist of telling burns on hands, overheating and being fragile not being able to take multiple hits. Lasiks can range from handguns, swords and rifles
+Lasiks: Get your hands on one of these puppies. They're valuable, trustworthy and very beautiful. Just be careful with your hands they'll burn.
+""")
+
